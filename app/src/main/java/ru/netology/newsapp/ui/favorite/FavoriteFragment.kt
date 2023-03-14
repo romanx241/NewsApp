effb50f7a1c79c9b5ed7824d9965882b3190f8cd
romanx241 <hohlovr@mail.ru>
2023-03-14 19:24:12 +0300

    Lesson-2

---
+++ app/src/main/java/ru/netology/newsapp/ui/favorite/FavoriteFragment.kt
@@ -0,0 +1,28 @@
+package ru.netology.newsapp.ui.favorite
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.netology.newsapp.R
+
+class FavoriteFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_favorite, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/ru/netology/newsapp/ui/splash/SplashFragment.kt
@@ -0,0 +1,20 @@
+package ru.netology.newsapp.ui.splash
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.netology.newsapp.R
+
+
+class SplashFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+}
